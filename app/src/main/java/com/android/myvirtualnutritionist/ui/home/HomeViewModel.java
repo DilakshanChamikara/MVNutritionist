438710a26e5ae071c31161fb60bc77fe2279b0ee
Dilakshan <wvdilakshanchamikara@gmail.com>
2021-05-10 22:47:09 +0530

    Project updated

--- app/src/main/java/com/android/myvirtualnutritionist/ui/home/HomeViewModel.java
+++ app/src/main/java/com/android/myvirtualnutritionist/ui/home/HomeViewModel.java
@@ -6,14 +6,24 @@ import androidx.lifecycle.ViewModel;
 
 public class HomeViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private MutableLiveData<String> caloriesCalc;
+    private MutableLiveData<String> caloriesDesc;
 
     public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is Home fragment");
+        caloriesCalc = new MutableLiveData<>();
+        caloriesCalc.setValue("2073  -  0  +  0  =  2073 ");
+
+        caloriesDesc = new MutableLiveData<>();
+        caloriesDesc.setValue("\tGoal\t\t\t\t\t\t\t\tFood\t\t\t\t\tExercise\t\t\t\t\tRemaining");
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<String> getCaloriesCalc() {
+        return caloriesCalc;
     }
+
+    public LiveData<String> getCaloriesDesc(){
+        return caloriesDesc;
+    }
+
+
 }
