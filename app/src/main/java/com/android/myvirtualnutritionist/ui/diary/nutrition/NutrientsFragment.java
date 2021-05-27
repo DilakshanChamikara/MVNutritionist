780e6c0ea46660d65428e630df8c6d37ca84c410
DilakshanChamikara <wvdilakshanchamikara@gmail.com>
2021-05-28 01:59:26 +0530

    Project updated

--- app/src/main/java/com/android/myvirtualnutritionist/ui/diary/nutrition/NutrientsFragment.java
+++ app/src/main/java/com/android/myvirtualnutritionist/ui/diary/nutrition/NutrientsFragment.java
@@ -8,12 +8,20 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.myvirtualnutritionist.R;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NutrientsFragment extends Fragment {
+
+    RecyclerView tableBodyRecyclerview;
+    NutrientsTableAdapter nutrientsTableAdapter;
 
     @Nullable
     @org.jetbrains.annotations.Nullable
@@ -21,7 +29,29 @@ public class NutrientsFragment extends Fragment {
     public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_nutrients, container, false);
 
+        tableBodyRecyclerview = view.findViewById(R.id.tableBodyRecyclerview);
+        setRecyclerView();
 
         return view;
+    }
+
+    private void setRecyclerView() {
+        tableBodyRecyclerview.setHasFixedSize(true);
+        tableBodyRecyclerview.setLayoutManager(new LinearLayoutManager(getActivity()));
+        nutrientsTableAdapter = new NutrientsTableAdapter(getActivity(), getList());
+        tableBodyRecyclerview.setAdapter(nutrientsTableAdapter);
+    }
+
+    private List<NutrientsTableDataModel> getList() {
+
+        List<NutrientsTableDataModel> dataModelList = new ArrayList<>();
+
+        dataModelList.add(new NutrientsTableDataModel("Protein", "0g", "104g", "104g"));
+        dataModelList.add(new NutrientsTableDataModel("Carbohydrates", "0g", "1259g", "1259g"));
+        dataModelList.add(new NutrientsTableDataModel("Fiber", "0g", "38g", "38g"));
+        dataModelList.add(new NutrientsTableDataModel("Sugar", "0g", "78g", "78g"));
+        dataModelList.add(new NutrientsTableDataModel("Fat", "0g", "69g", "69g"));
+
+        return dataModelList;
     }
 }
--- app/src/main/java/com/android/myvirtualnutritionist/ui/diary/nutrition/NutrientsTableAdapter.java
+++ app/src/main/java/com/android/myvirtualnutritionist/ui/diary/nutrition/NutrientsTableAdapter.java
@@ -1,11 +1,15 @@
 package com.android.myvirtualnutritionist.ui.diary.nutrition;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.myvirtualnutritionist.R;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -25,22 +29,45 @@ public class NutrientsTableAdapter extends RecyclerView.Adapter<NutrientsTableAd
     @NotNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
-        return null;
+
+        View view = LayoutInflater.from(context).inflate(R.layout.item_layout_nutrients_table, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull @NotNull ViewHolder holder, int position) {
 
+        if (nutrients_dataList != null && nutrients_dataList.size() > 0) {
+
+            NutrientsTableDataModel model = nutrients_dataList.get(position);
+            holder.tableBodyDataNutrients.setText(model.getNutrientsName());
+            holder.tableBodyDataTotal.setText(model.getTotal());
+            holder.tableBodyDataGoal.setText(model.getGoal());
+            holder.tableBodyDataLeft.setText(model.getLeft());
+        } else {
+            return;
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return nutrients_dataList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tableBodyDataNutrients;
+        TextView tableBodyDataTotal;
+        TextView tableBodyDataGoal;
+        TextView tableBodyDataLeft;
+
         public ViewHolder(@NonNull @NotNull View itemView) {
             super(itemView);
+
+            tableBodyDataNutrients = itemView.findViewById(R.id.tableBodyDataNutrients);
+            tableBodyDataTotal = itemView.findViewById(R.id.tableBodyDataTotal);
+            tableBodyDataGoal = itemView.findViewById(R.id.tableBodyDataGoal);
+            tableBodyDataLeft = itemView.findViewById(R.id.tableBodyDataLeft);
         }
     }
 }
