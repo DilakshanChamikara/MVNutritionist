ac3da470e098a2694dbca96b2da7104ff1244192
DilakshanChamikara <wvdilakshanchamikara@gmail.com>
2021-09-10 13:55:35 +0530

    Project updated

--- app/src/main/java/com/android/myvirtualnutritionist/ui/me/MeFragment.java
+++ app/src/main/java/com/android/myvirtualnutritionist/ui/me/MeFragment.java
@@ -1,5 +1,6 @@
 package com.android.myvirtualnutritionist.ui.me;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,11 +14,16 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.android.myvirtualnutritionist.FirstActivity;
 import com.android.myvirtualnutritionist.R;
 
 public class MeFragment extends Fragment {
 
     private MeViewModel meViewModel;
+
+    double heightCM, heightM, weightKG;
+    double BMI = 0.0;
+    String BMIWeightStatus;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         meViewModel = new ViewModelProvider(this).get(MeViewModel.class);
@@ -56,44 +62,47 @@ public class MeFragment extends Fragment {
         });
 
         final TextView age = root.findViewById(R.id.text_meAge);
-        meViewModel.getAge().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(String s) {
-                age.setText(s);
-            }
-        });
-
         final TextView gender = root.findViewById(R.id.text_meGender);
-        meViewModel.getGender().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                gender.setText(s);
-            }
-        });
-
-        final TextView diseaseType = root.findViewById(R.id.text_meDiseaseType);
-        meViewModel.getDiseaseType().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                diseaseType.setText(s);
-            }
-        });
-
         final TextView height = root.findViewById(R.id.text_meHeight);
-        meViewModel.getHeight().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(String s) {
-                height.setText(s);
-            }
-        });
-
         final TextView weight = root.findViewById(R.id.text_meWeight);
-        meViewModel.getWeight().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(String s) {
-                weight.setText(s);
-            }
-        });
+        final TextView textBmi = root.findViewById(R.id.text_meBmi);
+        final TextView weightStatus = root.findViewById(R.id.text_meWeightStatus);
+
+        Intent intent = getActivity().getIntent();
+        int passedAgeValue = intent.getIntExtra(FirstActivity.EXTRA_AGE, 0);
+        double passedHeightCM = intent.getDoubleExtra(FirstActivity.EXTRA_HEIGHT, 0);
+        double passedWeightKG = intent.getDoubleExtra(FirstActivity.EXTRA_WEIGHT, 0);
+        String passedGender = intent.getStringExtra(FirstActivity.EXTRA_GENDER);
+
+        /**
+         * Body mass index **/
+        heightCM = passedHeightCM;
+        weightKG = passedWeightKG;
+        heightM = heightCM / 100;
+        BMI = (weightKG / ((heightM) * (heightM)));
+
+        /**
+         * Weight Status
+         * Below 18.5	Underweight
+         * 18.5—24.9	Healthy
+         * 25.0—29.9	Overweight
+         * 30.0 and Above	Obese
+         * **/
+        if (BMI < 18.5)
+            BMIWeightStatus = "Underweight";
+        else if (BMI >= 18.5 && BMI <= 24.9)
+            BMIWeightStatus = "Healthy";
+        else if (BMI >= 25.0 && BMI <= 29.9)
+            BMIWeightStatus = "Overweight";
+        else if (BMI >= 30.0)
+            BMIWeightStatus = "Obese";
+
+        age.setText("" + passedAgeValue);
+        gender.setText("" + passedGender);
+        height.setText("" + passedHeightCM);
+        weight.setText("" + passedWeightKG);
+        textBmi.setText("" + String.format("%.2f", BMI));
+        weightStatus.setText(BMIWeightStatus);
 
         return root;
     }
--- app/src/main/java/com/android/myvirtualnutritionist/ui/me/MeViewModel.java
+++ app/src/main/java/com/android/myvirtualnutritionist/ui/me/MeViewModel.java
@@ -12,11 +12,6 @@ public class MeViewModel extends ViewModel {
     private MutableLiveData<String> status;
     private MutableLiveData<String> fName;
     private MutableLiveData<String> lName;
-    private MutableLiveData<String> age;
-    private MutableLiveData<String> gender;
-    private MutableLiveData<String> diseaseType;
-    private MutableLiveData<String> height;
-    private MutableLiveData<String> weight;
 
     public MeViewModel() {
         meProfilePic = new MutableLiveData<>();
@@ -30,21 +25,6 @@ public class MeViewModel extends ViewModel {
 
         lName = new MutableLiveData<>();
         lName.setValue("Bandara");
-
-        age = new MutableLiveData<>();
-        age.setValue("23");
-
-        gender = new MutableLiveData<>();
-        gender.setValue("Female");
-
-        diseaseType = new MutableLiveData<>();
-        diseaseType.setValue("CardioVascularType-1");
-
-        height = new MutableLiveData<>();
-        height.setValue("125");
-
-        weight = new MutableLiveData<>();
-        weight.setValue("52");
     }
 
     public LiveData<Integer> getMeProfilePic() {
@@ -63,23 +43,4 @@ public class MeViewModel extends ViewModel {
         return lName;
     }
 
-    public LiveData<String> getAge() {
-        return age;
-    }
-
-    public LiveData<String> getGender() {
-        return gender;
-    }
-
-    public LiveData<String> getDiseaseType() {
-        return diseaseType;
-    }
-
-    public LiveData<String> getHeight() {
-        return height;
-    }
-
-    public LiveData<String> getWeight() {
-        return weight;
-    }
 }
